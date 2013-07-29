f83bcc95f5649047187fce28c1d9617ee76a1f6e
Tom "Landric" Blount <tb12g09@ecs.soton.ac.uk>
2013-07-29 13:31:25 +0100

    Parameterised display code
    Shuffled player choices each playthrough

--- static/js/game.js
+++ static/js/game.js
@@ -1,12 +1,68 @@
 $(document).ready(function(){
-    $('#no-js').remove();
     
+    //Code adapted from http://bost.ocks.org/mike/shuffle/
+    function shuffle(array)
+    {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex)
+        {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+    
+    function displayChoices(container, paragraph, choices, special_choices)
+    {
+        container.fadeOut(700);
+        container.empty();
+        
+        paragraph = '<br/><p>'+paragraph+'</p><br/>';
+        var final_choices = new Array();
+        
+        var length = choices.length,
+        element = null;
+        for (var i = 0; i < length; i++) {
+            element = choices[i];
+            final_choices.push('<div class="span8 choice"><p>'+element+'</p></div>');
+        }
+        
+        var length = special_choices.length,
+        element = null;
+        for (var i = 0; i < length; i++) {
+            element = special_choices[i];
+            final_choices.push('<div class="span8 choice special-choice"><p>'+element+'</p></div>');
+        }
+        
+        container.append($(paragraph));
+        shuffle(final_choices);
+        
+        var length = final_choices.length,
+        element = null;
+        for (var i = 0; i < length; i++) {
+            element = final_choices[i];
+            container.append($(element));
+        }
+        
+        container.fadeIn(1500);
+    }
+    
+    $('#no-js').remove();
     var container = $('<div class="span10 lead" id="main">').hide();
-    container.append($('<br/><p>Hi there! Let\'s get started - what\'s your favourite colour?</p><br/>'));
-    container.append($('<div class="span8 choice"><p>Red</p></div>'));
-    container.append($('<div class="span8 choice"><p>Green</p></div>'));
-    container.append($('<div class="span8 choice"><p>Blue</p></div>'));
-    container.append($('<div class="span8 choice special-choice"><p>I choose Octarine!</p></div>'));
     $('#content').append(container);
-    container.fadeIn(1500);
+    
+    var paragraph = "Hi there! Let's get started - what's your favourite colour?"
+    var choices = ["Red", "Green", "Blue"];
+    var special_choices = ["I choose Octarine!"];
+    displayChoices(container, paragraph, choices, special_choices);
 });
+
