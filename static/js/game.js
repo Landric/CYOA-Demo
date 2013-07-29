b1bd7dc127e6e9a79328f050ca6357eb62ba68fd
Tom "Landric" Blount <tb12g09@ecs.soton.ac.uk>
2013-07-29 17:15:32 +0100

    Pass objects instead of arrays
    This allows for storing consequences (functions) with each player
    choice.
    Currently this just displays an alert then hides the container.

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,7 @@
+$('#no-js').remove();
+
 $(document).ready(function(){
-    
+
     //Code adapted from http://bost.ocks.org/mike/shuffle/
     function shuffle(array)
     {
@@ -21,48 +23,41 @@ $(document).ready(function(){
         return array;
     }
     
+    function createCallback(consequence){
+        return function(){
+            consequence();
+            container.fadeOut(700, container.empty);
+        }
+    }
+    
     function displayChoices(container, paragraph, choices, special_choices)
     {
-        container.fadeOut(700);
-        container.empty();
-        
         paragraph = '<br/><p>'+paragraph+'</p><br/>';
-        var final_choices = new Array();
+        var final_choices = new Object();
         
-        var length = choices.length,
-        element = null;
-        for (var i = 0; i < length; i++) {
-            element = choices[i];
-            final_choices.push('<div class="span8 choice"><p>'+element+'</p></div>');
+        for (choice in choices) {
+            final_choices['<div class="span8 choice"><p>'+choice+'</p></div>'] = choices[choice];
         }
         
-        var length = special_choices.length,
-        element = null;
-        for (var i = 0; i < length; i++) {
-            element = special_choices[i];
-            final_choices.push('<div class="span8 choice special-choice"><p>'+element+'</p></div>');
+        for (choice in special_choices) {
+            final_choices['<div class="span8 choice special-choice"><p>'+choice+'</p></div>'] = special_choices[choice];
         }
         
         container.append($(paragraph));
-        shuffle(final_choices);
+        //shuffle(final_choices);
         
-        var length = final_choices.length,
-        element = null;
-        for (var i = 0; i < length; i++) {
-            element = final_choices[i];
-            container.append($(element));
+        for (choice in final_choices) {
+            container.append($(choice).click(createCallback(final_choices[choice])));
         }
         
         container.fadeIn(1500);
     }
     
-    $('#no-js').remove();
     var container = $('<div class="span10 lead" id="main">').hide();
     $('#content').append(container);
     
     var paragraph = "Hi there! Let's get started - what's your favourite colour?"
-    var choices = ["Red", "Green", "Blue"];
-    var special_choices = ["I choose Octarine!"];
+    var choices = {"Red" : function(){alert("Red!")}, "Green" : function(){alert("Green!")}, "Blue" : function(){alert("Blue!")}};
+    var special_choices = {"I choose Octarine!" : function(){alert("Wizard!")}};
     displayChoices(container, paragraph, choices, special_choices);
 });
-
